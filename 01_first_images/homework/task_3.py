5af678071d9cc6d3397cbc8e4bec640ea08662fc
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-02-19 02:49:27 +0300

    complete task_03 in homework_1

--- 01_first_images/homework/task_3.py
+++ 01_first_images/homework/task_3.py
@@ -1,33 +1,141 @@
 import cv2
 import numpy as np
 
+Point = tuple[int, int]
 
-def rotate(image, point: tuple, angle: float) -> np.ndarray:
+
+def Rotate(image: np.ndarray, point: tuple, angle: float) -> np.ndarray:
     """
-    Повернуть изображение по часовой стрелке на угол от 0 до 360 градусов и преобразовать размер изображения.
+    Поворачивает изображение по часовой стрелке на угол от 0 до 360 градусов
+    и меняет размер изображения.
 
-    :param image: исходное изображение
-    :param point: значение точки (x, y), вокруг которой повернуть изображение
-    :param angle: угол поворота
-    :return: повернутное изображение
+    Args:
+        image (np.ndarray): исходной изображение (матрица цветов) для поворота.
+        point (tuple): координаты центра вращения.
+        angle (float): угол поворота в градусах: от 0 до 360.
+
+    Raises:
+        ValueError: Rotate: angle should be between 0 and 360.
+
+    Returns:
+        np.ndarray: повернутое изображение с измененным размером.
     """
-    # Ваш код
-    pass
 
-    return image
+    if angle > 360 or angle < 0:
+        raise ValueError("Rotate: angle should be between 0 and 360.")
+
+    while angle > 90:
+        image = Rotate(image, point, 90)
+        angle -= 90
+
+    height, width, _ = image.shape
+    angle = np.deg2rad(angle)
+
+    transition_matrix = np.matrix(
+        [[np.sin(angle), np.cos(angle)],
+         [np.cos(angle), np.sin(angle)]]
+    )
+
+    new_width, new_height = np.ceil(transition_matrix * np.array([[height], [width]]))
+    new_width, new_height = int(new_width), int(new_height)
+
+    src_pos = np.float32(np.array(
+        [[0, 0],
+         [0, height],
+         [width, 0]]
+    ))
+
+    dst_pos = np.float32(np.array(
+        [[0, width * np.sin(angle)],
+         [height * np.sin(angle), new_height],
+         [width * np.cos(angle), 0]]
+    ))
+
+    return cv2.warpAffine(image.copy(),
+                          cv2.getAffineTransform(src_pos, dst_pos),  # type: ignore
+                          (new_width, new_height))  # type: ignore
 
 
-def apply_warpAffine(image, points1, points2) -> np.ndarray:
+def FindCorners(mask: np.ndarray) -> list[Point]:
     """
-    Применить афинное преобразование согласно переходу точек points1 -> points2 и
-    преобразовать размер изображения.
+    Находит четыре угла бинарной маски (тетради).
 
-    :param image:
-    :param points1:
-    :param points2:
-    :return: преобразованное изображение
+    Ищет первый ненулевой пиксель вдоль каждой границы маски.
+    Углы возвращаются в следующем порядке:
+    верхний левый, нижний левый, верхний правый, нижний правый.
+
+    Args:
+        mask (np.ndarray): бинарная маска.
+
+    Returns:
+        list[Point]: список из четырех кортежей (x, y), представляющих координаты углов.
     """
-    # Ваш код
-    pass
 
-    return image
+    corners: list[Point] = []
+
+    for is_transposed in [False, True]:
+        array = np.transpose(mask) if is_transposed \
+            else mask
+
+        for step in (1, -1):
+            start = 0 if step > 0 \
+                else len(array) - 1
+
+            stop = len(array) if step > 0 \
+                else -1
+
+            corner = (-1, -1)
+
+            for index in range(start, stop, step):
+                row = array[index]
+                if np.any(row):
+                    value_index = np.nonzero(row)[0][0]
+                    corner = (value_index, index) if is_transposed \
+                        else (index, value_index)
+
+                    break  # первый угол найден
+
+            corners.append(corner)
+
+    return corners
+
+
+def FindNotebookImage(image: np.ndarray) -> np.ndarray:
+    """
+    Преобразует изображение, чтобы получить вид сверху, 
+    как если бы это была отсканированная тетрадь.
+
+    Функция использует перспективное преобразование для выравнивания изображения,
+    предполагая, что в изображении есть красные маркеры по углам, которые указывают
+    область тетради.  
+    Она ищет эти маркеры, определяет их координаты и использует их для преобразования перспективы.
+
+    Args:
+        image (np.ndarray): исходной изображение (матрица цветов).
+
+    Returns:
+        np.ndarray: выходное изображение.
+    """
+
+    red_high, red_low = (6.0, 255, 255), (0.0, 0, 0)
+    height, width, _ = image.shape
+
+    src_pos = np.float32([[elem[1], elem[0]]
+                          for elem in FindCorners(
+                              cv2.inRange(cv2.cvtColor(image,   # type: ignore
+                                                       cv2.COLOR_RGB2HSV),
+                                          red_low,              # type: ignore
+                                          red_high))])          # type: ignore
+
+    dst_pos = np.float32(
+        [[0, 0],  # type: ignore
+         [width, height],
+         [0, height],
+         [width, 0]]
+    )
+
+    return cv2.warpPerspective(image.copy(),
+                               cv2.getPerspectiveTransform(
+        src_pos, dst_pos),  # type: ignore
+        (width, height)
+    )
